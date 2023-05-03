6b55b38bb07a131a438ebe980f361e55305a6bb5
skydreamer21 <skydreamer210@gmail.com>
2023-05-03 15:59:57 +0900

    feat: Place 관련 Mapper, xml 작성

--- src/main/java/com/discovero/enjoytrip/place/model/PlaceMapper.java
+++ src/main/java/com/discovero/enjoytrip/place/model/PlaceMapper.java
@@ -1,8 +1,15 @@
 package com.discovero.enjoytrip.place.model;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface PlaceMapper {
-
+	void insertReview(PlaceReviewDto prDto);
+	void deleteReviewById(int reviewId);
+	List<PlaceReviewDto> selectAllPlaceReview();
+	HotPlaceDto selectHotPlaceByContentId(int contentId);
+	void updateReviewCount(@Param("contentId") int contentId, @Param("updateValue") int updateValue);
 }
