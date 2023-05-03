65da2e6d823a7c04ce3a86915c5e18a80f9408bd
skydreamer21 <skydreamer210@gmail.com>
2023-05-03 16:26:42 +0900

    feat: PlaceService 구현

--- src/main/java/com/discovero/enjoytrip/place/model/IPlaceService.java
+++ src/main/java/com/discovero/enjoytrip/place/model/IPlaceService.java
@@ -1,5 +1,10 @@
 package com.discovero.enjoytrip.place.model;
 
-public interface IPlaceService {
+import java.util.List;
 
+public interface IPlaceService {
+	void writeReview(PlaceReviewDto prDto);
+	void deleteReviewById(int reviewId);
+	List<PlaceReviewDto> findtAllPlaceReview();
+	List<HotPlaceDto> findTopNPlace(int count);
 }
--- src/main/java/com/discovero/enjoytrip/place/model/PlaceServiceImpl.java
+++ src/main/java/com/discovero/enjoytrip/place/model/PlaceServiceImpl.java
@@ -1,9 +1,13 @@
 package com.discovero.enjoytrip.place.model;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlaceServiceImpl implements IPlaceService {
+	static final int INCREASE = 1;
+	static final int DECREASE = -1;
 	
 	private PlaceMapper placeMapper;
 	
@@ -11,4 +15,28 @@ public class PlaceServiceImpl implements IPlaceService {
 		super();
 		this.placeMapper = placeMapper;
 	}
+
+	@Override
+	public void writeReview(PlaceReviewDto prDto) {
+		placeMapper.insertReview(prDto);
+		placeMapper.updateHotPlaceReviewCountById(prDto.getReview_id(), INCREASE);
+	}
+
+	@Override
+	public void deleteReviewById(int reviewId) {
+		placeMapper.deleteReviewById(reviewId);
+		placeMapper.updateHotPlaceReviewCountById(reviewId, DECREASE);
+	}
+
+	@Override
+	public List<PlaceReviewDto> findtAllPlaceReview() {
+		return placeMapper.selectAllPlaceReview();
+	}
+
+	@Override
+	public List<HotPlaceDto> findTopNPlace(int count) {
+		return placeMapper.selectTopNPlace(count);
+	}
+	
+	
 }
