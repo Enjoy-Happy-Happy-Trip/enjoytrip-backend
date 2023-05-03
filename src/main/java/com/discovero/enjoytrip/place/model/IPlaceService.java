e579b77fb0157056181f416bca85a2a32d60583e
skydreamer21 <skydreamer210@gmail.com>
2023-05-03 16:42:48 +0900

    fix: findTopPlace Attraction List 반환하도록 수정

--- src/main/java/com/discovero/enjoytrip/place/model/IPlaceService.java
+++ src/main/java/com/discovero/enjoytrip/place/model/IPlaceService.java
@@ -2,9 +2,11 @@ package com.discovero.enjoytrip.place.model;
 
 import java.util.List;
 
+import com.discovero.enjoytrip.attraction.model.AttractionDto;
+
 public interface IPlaceService {
 	void writeReview(PlaceReviewDto prDto);
 	void deleteReviewById(int reviewId);
 	List<PlaceReviewDto> findtAllPlaceReview();
-	List<HotPlaceDto> findTopNPlace(int count);
+	List<AttractionDto> findTopPlace();
 }
--- src/main/java/com/discovero/enjoytrip/place/model/PlaceServiceImpl.java
+++ src/main/java/com/discovero/enjoytrip/place/model/PlaceServiceImpl.java
@@ -1,19 +1,26 @@
 package com.discovero.enjoytrip.place.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+
+import com.discovero.enjoytrip.attraction.model.AttractionDto;
+import com.discovero.enjoytrip.attraction.model.AttractionMapper;
 
 @Service
 public class PlaceServiceImpl implements IPlaceService {
 	static final int INCREASE = 1;
 	static final int DECREASE = -1;
+	static final int HOT_PLACE_CNT = 100;
 	
 	private PlaceMapper placeMapper;
+	private AttractionMapper attractionMapper;
 	
-	public PlaceServiceImpl(PlaceMapper placeMapper) {
+	public PlaceServiceImpl(PlaceMapper placeMapper, AttractionMapper attractionMapper) {
 		super();
 		this.placeMapper = placeMapper;
+		this.attractionMapper = attractionMapper;
 	}
 
 	@Override
@@ -34,9 +41,10 @@ public class PlaceServiceImpl implements IPlaceService {
 	}
 
 	@Override
-	public List<HotPlaceDto> findTopNPlace(int count) {
-		return placeMapper.selectTopNPlace(count);
+	public List<AttractionDto> findTopPlace() {
+		List<HotPlaceDto> hotList = placeMapper.selectTopNPlace(HOT_PLACE_CNT);
+		return hotList.stream()
+				.map(hotplace -> attractionMapper.selectAttractionById(hotplace.getContent_id()))
+				.collect(Collectors.toList());
 	}
-	
-	
 }
