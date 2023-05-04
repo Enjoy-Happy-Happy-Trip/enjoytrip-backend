db03b41cc3a052dd24630929e400b34d18986468
skydreamer21 <skydreamer210@gmail.com>
2023-05-04 14:58:11 +0900

    PlaceController user_id dto 설정 및 rest 로 수정

--- src/main/java/com/discovero/enjoytrip/place/controller/PlaceController.java
+++ src/main/java/com/discovero/enjoytrip/place/controller/PlaceController.java
@@ -2,17 +2,22 @@ package com.discovero.enjoytrip.place.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.discovero.enjoytrip.attraction.model.AttractionDto;
+import com.discovero.enjoytrip.member.model.MembersDto;
 import com.discovero.enjoytrip.place.model.IPlaceService;
 import com.discovero.enjoytrip.place.model.PlaceReviewDto;
 
@@ -30,11 +35,15 @@ public class PlaceController {
 	
 	
 	@PostMapping("/writereview")
-	public String write(PlaceReviewDto prDto) {
-		logger.debug("POST write called");
+	@ResponseBody
+	public ResponseEntity<String> write(HttpSession session, @RequestBody PlaceReviewDto prDto) {
+		logger.info("POST write called");
+		MembersDto login = (MembersDto) session.getAttribute("login");
+		prDto.setUser_id(login.getUser_id());
+		logger.info("received review : {}", prDto);
 		placeService.writeReview(prDto);
 		// TODO: review 게시판 구현시 변경하기
-		return "redirect:/";
+		return ResponseEntity.ok("review was written successfully!");
 	}
 	
 	@GetMapping("/reviewlist")
