8d80771dfbe5f9311dc66219389a662439edd6e2
sungjun <tjdwnsmm@naver.com>
2023-05-19 17:44:02 +0900

    fix: userplandto for saveplan created

--- src/main/java/com/discovero/enjoytrip/tour/model/ITourService.java
+++ src/main/java/com/discovero/enjoytrip/tour/model/ITourService.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface ITourService {
 	List<TourDto> tourlist(int sido_code, int content_type_id, String word);
 	TourDto tourDetail(int article_no);
-	void savePlan(String[] placeNames, String user_id, String plan_title, String start_date, String end_date);
+	void savePlan(String[] placeNames, int[] content_ids, String user_id, String plan_title, String start_date, String end_date);
 	List<SidoGugunCodeDto> getSido();
 	List<SidoGugunCodeDto> getGugunInSido(String sido);
 }
