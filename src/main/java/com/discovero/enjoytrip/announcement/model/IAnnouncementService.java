8340f99b80d3219cca06f93236f121c6f7303eed
skydreamer21 <skydreamer210@gmail.com>
2023-05-23 21:13:18 +0900

    feat: add announcement service interface methods

--- src/main/java/com/discovero/enjoytrip/announcement/model/IAnnouncementService.java
+++ src/main/java/com/discovero/enjoytrip/announcement/model/IAnnouncementService.java
@@ -1,5 +1,17 @@
 package com.discovero.enjoytrip.announcement.model;
 
+import java.util.List;
+
 public interface IAnnouncementService {
+
+	List<AnnouncementDto> findAllAnouncements();
+
+	AnnouncementDto findAnnouncementById(int announcementId, boolean isViewed);
+
+	void addAnnouncement(AnnouncementDto adto);
+
+	void modifyAnnouncementById(AnnouncementDto adto);
+
+	void removeAnnouncementById(int announcementId);
 
 }
