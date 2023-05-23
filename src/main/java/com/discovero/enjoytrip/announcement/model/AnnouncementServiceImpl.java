e3acea06389cec9d4566ff57d1b9b59d1674ecc4
skydreamer21 <skydreamer210@gmail.com>
2023-05-23 21:24:52 +0900

    feat: implement Announcement Service with CRUD operations

--- src/main/java/com/discovero/enjoytrip/announcement/model/AnnouncementServiceImpl.java
+++ src/main/java/com/discovero/enjoytrip/announcement/model/AnnouncementServiceImpl.java
@@ -1,8 +1,50 @@
 package com.discovero.enjoytrip.announcement.model;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AnnouncementServiceImpl implements IAnnouncementService {
 
+	private AnnouncementMapper announcementMapper;
+
+	public AnnouncementServiceImpl(AnnouncementMapper announcementMapper) {
+		super();
+		this.announcementMapper = announcementMapper;
+	}
+
+	@Override
+	public List<AnnouncementDto> findAllAnouncements() {
+		return announcementMapper.selectAnnouncements();
+	}
+
+	@Override
+	@Transactional
+	public AnnouncementDto findAnnouncementById(int announcementId, boolean isViewed) {
+		AnnouncementDto announcement = announcementMapper.selectAnnouncementById(announcementId);
+		if (isViewed) {
+			announcementMapper.updateHitByAddingOneById(announcementId);
+		}
+		return announcement;
+	}
+
+	@Override
+	public void addAnnouncement(AnnouncementDto adto) {
+		announcementMapper.insertAnnouncement(adto);
+		
+	}
+
+	@Override
+	public void modifyAnnouncementById(AnnouncementDto adto) {
+		announcementMapper.updateAnnouncementById(adto);
+		
+	}
+
+	@Override
+	public void removeAnnouncementById(int announcementId) {
+		announcementMapper.deleteAnnouncementById(announcementId);
+	}
+	
 }
