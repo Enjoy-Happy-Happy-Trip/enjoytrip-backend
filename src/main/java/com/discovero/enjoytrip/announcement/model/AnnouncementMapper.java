6f49fd314cbf6d0e4ce5ff863e91e9a0a3c02093
skydreamer21 <skydreamer210@gmail.com>
2023-05-23 21:26:52 +0900

    feat: add methods including CRUD operationsto Announcement Mapper

--- src/main/java/com/discovero/enjoytrip/announcement/model/AnnouncementMapper.java
+++ src/main/java/com/discovero/enjoytrip/announcement/model/AnnouncementMapper.java
@@ -1,8 +1,22 @@
 package com.discovero.enjoytrip.announcement.model;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class AnnouncementMapper {
+public interface AnnouncementMapper {
+
+	List<AnnouncementDto> selectAnnouncements();
+
+	AnnouncementDto selectAnnouncementById(int announcementId);
+
+	void updateHitByAddingOneById(int announcementId);
+
+	void insertAnnouncement(AnnouncementDto adto);
+
+	void updateAnnouncementById(AnnouncementDto adto);
+
+	void deleteAnnouncementById(int announcementId);
 
 }
